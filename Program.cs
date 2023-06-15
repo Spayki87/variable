5ffc38d9866fd3f97961514ab4dc18758a096db7
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-06-15 21:09:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // создание переменных
+            int money;// деньги
+            int age;//возраст
+            int life;//жизнь
+            int health;//здоровье
+            int height;//высота
+            string name;//имя
+            string sityName;//название города
+            string nameDogs;//имена собак
+            bool openDoor;//дверь открыта (истина или ложь)
+            bool lightOn;//включен ли свет (истина или ложь)
+        }
+    }
+}
