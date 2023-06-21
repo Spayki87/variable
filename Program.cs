9d0045190e5bacb45dbded06ad60fc0e46ad23d5
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-06-21 21:51:18 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace variable
+namespace Polyclinic__поликлиника_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-             int money;
-            uint height;
-            float armor;
-            short health;
-            char numberChoice;
-            string name;
-            bool haveDoor;
-            byte crystals;
-            ulong namberOfPeople;
-            double distance;
+            int oldLadiesInLine;
+            int timeOfReceipt = 10;
+            Console.WriteLine("Вы заходите в поликлинику и видите огромную очередь из старушек, посчитайте количество старушек.");
+            Console.Write("Введите количество старушек в очереди:");
+            oldLadiesInLine = Convert.ToInt32(Console.ReadLine());
+            int waitingTime = oldLadiesInLine * timeOfReceipt;
+            int minutesInHour = 60;
+            int waitingHours = waitingTime / minutesInHour;
+            int waitingMinutes = waitingTime % minutesInHour;
+            Console.WriteLine($"Вы должны отстоять в очереди {waitingHours} часа и {waitingMinutes} минут");
         }
     }
 }
